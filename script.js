55bc1639ad9fe699f00856c07150d4514fb5b11e
Nicolas Vivas <nicolasvivas93@gmail.com>
2022-08-04 16:12:50 -0300

    Create buttons for playing every round and show message of results

--- script.js
+++ script.js
@@ -19,33 +19,55 @@ function getComputerChoice() {
 
 //CREAR FUNCION playRound() 
 
-function playRound(playerSelection = prompt("Please choose (Rock-Paper-Scissors) to play:"), computerSelection = getComputerChoice()) {
+function playRound(playerSelection, computerSelection = getComputerChoice()) {
     //Convertir ambas strings en minusculas
     let playerChoice = playerSelection.toLowerCase();
     let computerChoice = computerSelection.toLowerCase();
+    
+    //Crear scores de jugadores
+    let playerScore = 0;
+    let computerScore = 0;
 
     if (playerChoice == "rock" && computerChoice == "paper") {
-        console.log("Winner: Computer (Paper beats Rock)");
+        showResults("Winner: Computer (Paper beats Rock)");
+        computerScore += 1;
     } else if (playerChoice == "rock" && computerChoice == "scissors") {
-        console.log("Winner: Player (Rock beats Scissors)");
+        showResults("Winner: Player (Rock beats Scissors)");
+        playerScore += 1;
     } else if (playerChoice == "paper" && computerChoice == "rock") {
-        console.log("Winner: Player (Paper beats Rock)");
+        showResults("Winner: Player (Paper beats Rock)");
+        playerScore += 1;
     } else if (playerChoice == "paper" && computerChoice == "scissors") {
-        console.log("Winner: Computer (Scissors beats Paper)");
+        showResults("Winner: Computer (Scissors beats Paper)");
+        computerScore += 1;
     } else if (playerChoice == "scissors" && computerChoice == "rock") {
-        console.log("Winner: Computer (Rock beats Scissors)");
+        showResults("Winner: Computer (Rock beats Scissors)");
+        computerScore += 1;
     } else if (playerChoice == "scissors" && computerChoice == "paper") {
-        console.log("Winner: Player (Scissors beats Paper)");
+        showResults("Winner: Player (Scissors beats Paper)");
+        playerScore += 1;
     } else {
-        console.log("Tied game")
+        showResults("Tied game")
     }
+
+    console.log(computerScore)
+    console.log(playerScore)
 }
 
-function game() {
-    for (let i = 1; i <= 5; i++) {
-        playRound();
-    }
+function showResults(content) {
+    const results = document.querySelector(".results")
+    const message = document.createElement("h2")
+    message.innerText = content
+    results.appendChild(message)
 }
 
-game();
 
+const buttons = document.querySelectorAll("button")
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        //traer texto del boton
+        let btnTxt = button.textContent
+        playRound(btnTxt)
+    })
+})
